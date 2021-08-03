cbcb0aa090a63ae0794109cb93b16847bd05b890
Matt Martin <matthew.i.martin3@gmail.com>
2021-08-02 18:56:06 -0700

    Changed timeline odd element text-alignment. Fixed email.

--- src/data.js
+++ src/data.js
@@ -3,7 +3,8 @@ module.exports = {
   header: "Hi, I'm Matt!",
   description: "I'm a software engineer with a passion for designing and building products for mobile and web that make our lives easier and more productive.",
   name: 'Matt Martin',
-  email: 'matthew.i.{LAST_NAME}3@gmail.com',
+  emailStart: 'matthew.',
+  emailEnd: 'i.{last_name}3@gmail.com',
   education: "Bachelor's in Computer Science, Washington State University, 2011",
   cta: {
     label: 'View my resumé',
@@ -19,7 +20,7 @@ module.exports = {
       date: '2015-Present',
       description: 'A freemium productivity app that allows Navy flight students and instructors to effectively manage their individual flying schedules. A Node backend hosted on Firebase crawls the Navy flight schedules website, waiting for the next day\'s schedule to be released for each of the 25 training squadrons.',
       description2: 'When a schedule drops, users receive a push notification that opens the app to their scheduled event(s). The frontend is a native iOS app that parses the raw HTML schedule data and allows users to search and filter by name and squadron.',
-      tags: ['iOS', 'Swift', 'Node.js', 'Firebase'],
+      tags: ['iOS', 'Swift', 'Node', 'Firebase'],
       kpis: ['Top 15 in "Productivity" apps from Feb 2020-March 2020.', 'Over 10,000 downloads since 2015.', '300 daily unique users', '90% Conversion Rate', 'Projected Android release in Fall \'21'],
       alt: 'Skeds Screenshot', // alt description of image for accessibility. defaults to '{{name}} Screenshot'
       img: 'skeds.png',
@@ -30,7 +31,7 @@ module.exports = {
       date: '2013-2015',
       description: 'I created Juke to solve the problem of playlist control at parties and events. The party host launches a lightweight node.js server that opens an endpoint on their local network. Users with mobile devices scan a QR code or navigate to a custom url and are presented with a frontend to add songs to a playlist.',
       description2: 'The server ingests songs as they\'re added to the playlist and forwards them to a Spotify-API that places them in a queue. Songs are played in FIFO order. The party hosts retains admin powers to veto requests and skip songs from their mobile device or the host machine.',
-      tags: ['js', 'Node', 'Jade', 'Spotify-API', 'HTML', 'CSS'],
+      tags: ['js', 'Node', 'Jade'],
       kpis: ['Working prototype marketed to Spotify in 2013 but was ultimately declined.', 'Juke was used by friends and colleagues at home and work for weddings and other events.', 'API was deprecated in 2015 and Juke died with it.'],
       alt: 'Juke screenshot', // alt description of image for accessibility. defaults to '{{name}} Screenshot'
       img: 'juke.png',
@@ -40,7 +41,7 @@ module.exports = {
       name: 'Tomauto',
       date: '2014',
       description: 'Tomauto is a Chrome extension that allows users to highlight a movie name on any webpage, right click it, and access that film\'s Rotten Tomatoes rating in the context menu.',
-      tags: ['js', 'jQuery', 'HTML', 'CSS'],
+      tags: ['js', 'jQuery'],
       alt: 'Tomauto Screenshot', // alt description of image for accessibility. defaults to '{{name}} Screenshot'
       img: 'tomauto.png',
       url: 'https://www.github.com/heloguy/tomauto'
@@ -50,7 +51,7 @@ module.exports = {
       date: '2013',
       description: 'KHANAAS is a fork of "FOAAS" that provides a web API for friends and co-workers to express friendly disdain for one another\'s actions in the form of Captain James Kirk or Mr. Spock cursing their name in classic Star Trek style.',
       description2: 'The API went viral from an HN post and received 50,000 unique impressions on its first day. After two years, the domain lapsed and it has since been forked and revived by other users.',
-      tags: ['js', 'Node', 'Firebase', 'HTML', 'CSS'],
+      tags: ['js', 'Node', 'AWS'],
       alt: 'KHANAAS screenshot', // alt description of image for accessibility. defaults to '{{name}} Screenshot'
       img: 'khaan.jpg',
       url: 'http://www.khanaas.com/'
@@ -70,12 +71,12 @@ module.exports = {
     {
       title: 'Software Engineer on Mobile/Web at Freelance',
       timeline: 'May 2011 - Present',
-      description: 'Projects ranging from small productivity life-hack apps to a military flight scheduling app with thousands of downloads and hundreds of daily users.',
+      description: 'Projects ranging from small productivity life-hack apps to a paid flight scheduling app with thousands of downloads and hundreds of daily users.',
       responsibilities: [
-        'Full-stack web.',
-        'iOS.',
-        'Android.',
-        'See projects below.'
+        'Full-stack web',
+        'iOS',
+        'Android',
+        '<a href="#projects">Go to projects</a>'
       ]
     },
     {
